d19aed1093a0ed7139ee1bc11704e6062b2b9f22
loigen <22104626@usc.edu.ph>
2024-10-06 14:40:30 +0800

    update

--- src/components/Tables.jsx
+++ src/components/Tables.jsx
@@ -1,15 +1,19 @@
-import React from 'react';
-import StudentTable from './StudentTable';
-import TeacherTable from './TeacherTable';
-import GradeTable from './GradeTable';
-import SectionTable from './SectionTable';
-import AddTable from './AddTable'; // Import AddTable component
+import React from "react";
+import StudentTable from "./StudentTable";
+import TeacherTable from "./TeacherTable";
+import GradeTable from "./GradeTable";
+import SectionTable from "./SectionTable";
+import AddTable from "./AddTable"; // Import AddTable component
 
 export default function Tables() {
   return (
-    <div className="p-4 bg-green-500 h-screen w-screen overflow-auto"> {/* Full height & width container with green background */}      
+    <div className="p-4 bg-green-500 h-screen w-screen overflow-auto">
+      {" "}
+      {/* Full height & width container with green background */}
       {/* Top row: AddTable, GradeTable, and SectionTable beside each other */}
-      <div className="flex justify-between mb-4 h-1/2 space-x-4"> {/* Flexbox layout with gap */} 
+      <div className="flex justify-between mb-4 h-1/2 space-x-4">
+        {" "}
+        {/* Flexbox layout with gap */}
         <div className="flex-1 overflow-hidden">
           <AddTable />
         </div>
@@ -20,9 +24,10 @@ export default function Tables() {
           <SectionTable />
         </div>
       </div>
-
       {/* Bottom row: StudentTable and TeacherTable beside each other */}
-      <div className="flex justify-between h-1/2 space-x-4"> {/* Flexbox layout with gap */}
+      <div className="flex justify-between h-1/2 space-x-4">
+        {" "}
+        {/* Flexbox layout with gap */}
         <div className="flex-1 overflow-hidden">
           <StudentTable />
         </div>
